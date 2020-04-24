d7e00112750c3eb135938a1d4957c4b120cc9a19
yuhongyang <506814597@qq.com>
2020-04-24 08:42:45 +0800

    2020-04-24

--- hello.py
+++ hello.py
@@ -477,3 +477,130 @@
 #     y = 5
 # dya1 = Weekday.s
 # print(dya1,dya1.value)
+
+# class Hello(object):
+#     def hello(self,name="world"):
+#         print('Hello,%s' % name)
+# h = Hello()
+# print(type(Hello))
+
+# def fn(self, name='world'): # 先定义函数
+#     print('Hello, %s.' % name)
+
+# Hello = type('Hello', (object,), dict(hello=fn)) # 创建Hello class
+# h = Hello()
+# print(h.hello(),type(h))
+# 错误处理
+# def foo():
+#     r = some_function()
+#     if r == (-1):
+#         return(-1)
+#     return r
+# def bar():
+#     r = foo()
+#     if r == (-1):
+#         print('Error')
+#     else:
+#         pass
+# bar()
+
+# try:
+#     print('try...')
+#     r = 10/2
+#     print('result:',r)
+# except ZeroDivisionError as e:
+#     print('except:',e)
+# finally:
+#     print('finally:')
+# print('END')
+
+# try:
+#     print('try...')
+#     r = 10/int('2')
+#     print('result:',r)
+# except ValueError as e:
+#     print('ValueError:',e)
+# except ZeroDivisionError as e:
+#     print('ZeroDivisionError:',e)
+# else:
+#     print('no error')
+# finally:
+#     print('finally...')
+# print('END')
+
+# try:
+#     foo()
+# except ValueError as e:
+#     print('ValueError:',e)
+# except UnicodeError as e:
+#     print('UnicodeError')
+# def foo(s):
+#     return 10/int(s)
+
+# def bar(s):
+#     return foo(s)*2
+
+# def main():
+#     try:
+#         bar('0')
+#     except Exception as e:
+#         print('Error:',e)
+#     finally:
+#         print('finally...')
+# import logging
+# def foo(s):
+#     return 10/int(s)
+# def bar(s):
+#     return foo(s) * 2
+# def main():
+#     try:
+#         bar('0')
+#     except Exception as e:
+#         logging.exception(e)
+# main()
+# print('END')
+
+# class FooError(ValueError):
+#     pass
+
+# def foo(s):
+#     n = int(s)
+#     if n ==0:
+#         raise FooError('invalid value: %s' %s)
+#     return 10 / n
+# foo('0')
+
+# def foo(s):
+#     n = int(s)
+#     if n ==0:
+#         raise ValueError('invalid value: %s' %s)
+#     return 10 / n
+# def bar():
+#     try:
+#         foo('0')
+#     except ValueError as e:
+#         print('ValueError')
+#         raise
+# bar()
+
+# def foo(s):
+#     n = int(s)
+#     assert n != 0,'n is zero'
+#     return 10 /n
+# def main():
+#     foo('0')
+# main()
+
+# import logging
+# logging.basicConfig(level=logging.INFO)
+# s = '0'
+# n = int(s)
+# logging.info('n = %d' % n)
+# print(10 / n)
+
+import pdb
+
+s = '0'
+n = int(s)
+pdb.set_trace() # 运行到这里会自动暂停
+print(10 / n)
