854bbbb1902010c1ff9e7eaa6ef67f4a9d068738
yuhongyang <506814597@qq.com>
2020-04-24 11:18:58 +0800

    32

--- hello.py
+++ hello.py
@@ -598,9 +598,9 @@
 # logging.info('n = %d' % n)
 # print(10 / n)
 
-import pdb
+# import pdb
 
-s = '0'
-n = int(s)
-pdb.set_trace() # 运行到这里会自动暂停
-print(10 / n)
+# s = '0'
+# n = int(s)
+# pdb.set_trace() # 运行到这里会自动暂停
+# print(10 / n)
